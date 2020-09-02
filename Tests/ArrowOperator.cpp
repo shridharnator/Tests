6116b21e451427c5ed2ca9af16fb1e5187a729d5
shridharnator <32407571+shridharnator@users.noreply.github.com>
2020-09-02 08:14:34 +0530

    Add project files.

---
+++ Tests/ArrowOperator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+class Entity {
+public:
+	void print() {
+		std::cout << "Hello World" << std::endl;
+	}
+};
+int main() {
+	Entity e;
+	e.print();
+	Entity* ptr = &e;
+	*ptr.print();
+	return 0;
+}
